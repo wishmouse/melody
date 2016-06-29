dd7576d1b50f28fc4b02fb1a84059b6bb7ad28d1
wishmouse <heidi.goosen@gmail.com>
2016-06-29 16:47:30 +1200

    rendering three to a page

--- public/index.js
+++ public/index.js
@@ -14,15 +14,38 @@ $(document).ready(function(){
       .end(function(err, response){
         //if statement for error/ response
         var query = JSON.parse(response.text)
-        var link = query.external_urls.spotify
-        var imageSmall = query.album.images[1].url
-        var imageLarge = query.album.images[0].url
-        var songName = query.name
-        var preview = query.preview_url
-        document.getElementById("songName").innerHTML = songName
-        document.getElementById("link").innerHTML = link
-        $("body").append("<img src="+ imageSmall + ">")
-        $("body").append("<source src="+ preview + ">")
+        var linkOne = query.items[0].external_urls.spotify
+        var imageSmallOne = query.items[0].album.images[1].url
+        var songNameOne = query.items[0].name
+        console.log("songNameOne :", songNameOne)
+        var previewOne = query.items[0].preview_url
+
+        var linkTwo = query.items[1].external_urls.spotify
+        var imageSmallTwo = query.items[1].album.images[1].url
+        var songNameTwo = query.items[1].name
+        console.log("songNameTwo : ", songNameTwo)
+        var previewTwo = query.items[1].preview_url
+
+        var linkThree = query.items[2].external_urls.spotify
+        var imageSmallThree = query.items[2].album.images[1].url
+        var songNameThree = query.items[2].name
+        console.log("songNameThree", songNameThree)
+        var previewThree = query.items[2].preview_url
+
+        document.getElementById("songNameOne").innerHTML = songNameOne
+        document.getElementById("linkOne").innerHTML = linkOne
+        $("body").append("<img src="+ imageSmallOne + " style='width:200;height:200'>")
+        $("body").append("<source src="+ previewOne + ">")
+
+        document.getElementById("songNameTwo").innerHTML = songNameTwo
+        document.getElementById("linkTwo").innerHTML = linkTwo
+        $("body").append("<img src="+ imageSmallTwo + " style='width:200;height:200'>")
+        $("body").append("<source src="+ previewTwo + ">")
+
+        document.getElementById("songNameThree").innerHTML = songNameThree
+        document.getElementById("linkThree").innerHTML = linkThree
+        $("body").append("<img src="+ imageSmallThree + " style='width:200;height:200'>")
+        $("body").append("<source src="+ previewThree + ">")
         // document.getElementById("preview").innerHTML = preview
       })
   })
